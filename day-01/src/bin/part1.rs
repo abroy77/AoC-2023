b90dbc433fa4ec3d94b3a9f193ebfd69239a0e8f
Abhishek Roy <abhishek.roy@perspectum.com>
2023-12-02 10:57:45 +0000

    day01 part1

---
+++ day-01/src/bin/part1.rs
@@ -0,0 +1,80 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("need argument to filepath")
+    }
+
+    let mut sum = 0;
+    if let Ok(lines) = read_lines(&args[1]) {
+        for line in lines {
+            if let Ok(ip) = line {
+                sum += str_2_num(&ip);
+            }
+        }
+    }
+
+    println!("Result is {sum}")
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn str_2_num(input_line: &str) -> u32 {
+    let digits: Vec<u32> = input_line.chars().filter_map(|c| c.to_digit(10)).collect();
+    let num_digits = digits.len();
+    if num_digits > 1 {
+        digits[0] * 10 + digits[num_digits - 1]
+    } else if num_digits == 1 {
+        digits[0] * 10 + digits[0]
+    } else {
+        0
+    }
+}
+
+// add tests
+#[cfg(test)]
+mod tests {
+    use crate::str_2_num;
+
+    #[test]
+    fn str_2_num_1() {
+        assert_eq!((str_2_num("1abc2")), 12)
+    }
+
+    #[test]
+    fn str_2_num_2() {
+        assert_eq!((str_2_num("pqr3stu8vwx")), 38)
+    }
+
+    #[test]
+    fn str_2_num_3() {
+        assert_eq!((str_2_num("a1b2c3d4e5f")), 15)
+    }
+
+    #[test]
+    fn str_2_num_4() {
+        assert_eq!((str_2_num("treb7uchet")), 77)
+    }
+    #[test]
+    fn lines_test() {
+        let text = "1abc2
+                    pqr3stu8vwx
+                    a1b2c3d4e5f
+                    treb7uchet"
+            .to_string();
+        let mut sum = 0;
+        for line in text.lines() {
+            sum += str_2_num(&line);
+        }
+        assert_eq!(sum, 142)
+    }
+}
